c8cccdc78d5f35c494715ac1127daa2c177bba35
trainee <shweta.mangrule@capgemini.com>
2018-06-07 20:42:32 +0530

    with few errors

---
+++ src/main/java/com/app/springBoot/exception/ExceptionInBank.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.app.springBoot.exception;
+
+/**
+ * @author trainee
+ *
+ */
+public class ExceptionInBank extends Exception {
+	
+	public ExceptionInBank(String message) {
+		super(message);
+	}
+
+
+}
